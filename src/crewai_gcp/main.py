c764e79222e089d7d8fae29f9c4df8f2d079be6b
Future <ai.shaping.future@gmail.com>
2025-04-11 12:22:36 +0200

    Dex : new prompt

--- src/crewai_gcp/main.py
+++ src/crewai_gcp/main.py
@@ -38,15 +38,14 @@ def run():
     service = mailing.gmail_authenticate()
     email_list = ["nathanph.brigot@gmail.com","nathan.brigot@fr.ey.com"]
     # email_list = [
-    #     "nathanph.brigot@gmail.com",
+    #     "nathan.brigot@fr.ey.com",
     #     "lucas.treiber@fr.ey.com",
-    #     "joseph.harari@fr.ey.com",
     #     "hamid.filali@fr.ey.com",
     #     "emeline.lahaye@fr.ey.com",
-    #     "nathan.brigot@fr.ey.com",
-    #     "marin.lavigne@fr.ey.com",
     #     "charlotte.cazelles@fr.ey.com",
-    #     "aurelien.martin@fr.ey.com"
+    #     "aurelien.martin@fr.ey.com",
+    #     "ange.bernard@fr.ey.com",
+    #     "alexander.borisov@fr.ey.com"
     # ]
 
     for email in email_list:
